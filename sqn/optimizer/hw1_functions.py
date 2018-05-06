Fix Numerical Problem and add plot script

sqn/optimizer/hw1_functions.py
@@ -10,8 +10,9 @@ from math import exp
 def weird_func( x, order=0 ):
 
   # f(x) = x^4 + 6x^2 + 12(x-4)e^(x-1)
+#  print(x)
   value = pow(x, 4) + 6 * pow(x, 2) + 12 * (x - 4) * exp(x - 1)
-  
+
   if order==0:
       return value
   elif order==1:
@@ -30,13 +31,61 @@ def weird_func( x, order=0 ):
   else:
         raise ValueError("The argument \"order\" should be 0, 1 or 2")
 
+
+########################################################333
+def no_conv_func1(x, order=0):
+  '''x^4-4x^3-x^2+10x'''
+#  print(x)
+  value = pow(x, 4) - 4*pow(x, 3) - pow(x,2) + 10*x
+
+  if order==0:
+      return value
+  elif order==1:
+      # f'(x) = 4x^3 + 12x + 12(x-3)e^(x-1)
+      gradient = 4*pow(x, 3) - 12*pow(x, 2) - 2*x + 10
+
+      return (value, gradient)
+  elif order==2:
+      # f'(x) = 4x^3 + 12x + 12(x-3)e^(x-1)
+      gradient = 4*pow(x, 3) - 12*pow(x, 2) - 2*x + 10
+
+      # f''(x)= 12 (1 + e^(-1 + x) (-2 + x) + x^2)
+      hessian = 12*pow(x, 3) - 24*x - 2
+
+      return (value, gradient, hessian)
+  else:
+        raise ValueError("The argument \"order\" should be 0, 1 or 2")
+
+def no_conv_func2(x, order=0):
+  ''''''
+#  print(x)
+  value = pow(x, 6) - 2*pow(x, 5) + pow(x,3) - pow(x,2) + 3*x
+
+  if order==0:
+      return value
+  elif order==1:
+      # f'(x) = 4x^3 + 12x + 12(x-3)e^(x-1)
+      gradient = 6 * pow(x, 5) - 10*pow(x, 4) + 3*pow(x,2) - 2*x + 3
+
+      return (value, gradient)
+  elif order==2:
+      # f'(x) = 4x^3 + 12x + 12(x-3)e^(x-1)
+      gradient = 6 * pow(x, 5) - 10*pow(x, 4) + 3*pow(x,2) - 2*x + 3
+
+      # f''(x)= 12 (1 + e^(-1 + x) (-2 + x) + x^2)
+      hessian = 30*pow(x, 4) - 40*pow(x,3) + 6*x - 2
+
+      return (value, gradient, hessian)
+  else:
+        raise ValueError("The argument \"order\" should be 0, 1 or 2")
+
 ###############################################################################
 def boyd_example_func(x, order=0):
   a=np.matrix('1  3')
   b=np.matrix('1  -3')
   c=np.matrix('-1  0')
   x=np.asmatrix(x)
-  
+
   value = exp(a*x-0.1)+exp(b*x-0.1)+exp(c*x-0.1)
   if order==0:
       return value
@@ -53,7 +102,7 @@ def boyd_example_func(x, order=0):
 
 ###############################################################################
 def quadratic( H, b, x, order=0 ):
-    """ 
+    """
     Quadratic Objective
     H:          the Hessian matrix
     b:          the vector of linear coefficients
@@ -63,7 +112,7 @@ def quadratic( H, b, x, order=0 ):
     H = np.asmatrix(H)
     b = np.asmatrix(b)
     x = np.asmatrix(x)
-    
+
     value = 0.5 * x.T * H * x + b.T * x
 
     if order == 0:
@@ -81,7 +130,7 @@ def quadratic( H, b, x, order=0 ):
 
 ###############################################################################
 def draw_contour( func, gd_xs, newton_xs, fig, levels=np.arange(5, 1000, 10), x=np.arange(-5, 5.1, 0.05), y=np.arange(-5, 5.1, 0.05)):
-    """ 
+    """
     Draws a contour plot of given iterations for a function
     func:       the contour levels will be drawn based on the values of func
     gd_xs:      gradient descent iterates
@@ -95,30 +144,30 @@ def draw_contour( func, gd_xs, newton_xs, fig, levels=np.arange(5, 1000, 10), x=
     for i in range(len(x)):
         for j in range(len(y)):
             Z[i, j] = func( np.matrix([x[i],y[j]]).T , 0 )
-    
+
     plt.figure(fig)
     plt.contour( x, y, Z.T, levels, colors='0.75')
     plt.ion()
     plt.show()
-    
+
     line_gd, = plt.plot( gd_xs[0][0,0], gd_xs[0][1,0], linewidth=2, color='r', marker='o', label='GD' )
     line_newton, = plt.plot( newton_xs[0][0,0], newton_xs[0][1,0], linewidth=2, color='m', marker='o',label='Newton' )
-    
+
     L = plt.legend(handles=[line_gd,line_newton])
     plt.draw()
     time.sleep(1)
-    
+
     for i in range( 1, max(len(gd_xs), len(newton_xs)) ):
-        
+
         line_gd.set_xdata( np.append( line_gd.get_xdata(), gd_xs[ min(i,len(gd_xs)-1) ][0,0] ) )
         line_gd.set_ydata( np.append( line_gd.get_ydata(), gd_xs[ min(i,len(gd_xs)-1) ][1,0] ) )
-        
+
         line_newton.set_xdata( np.append( line_newton.get_xdata(), newton_xs[ min(i,len(newton_xs)-1) ][0,0] ) )
         line_newton.set_ydata( np.append( line_newton.get_ydata(), newton_xs[ min(i,len(newton_xs)-1) ][1,0] ) )
 
-        
+
         L.get_texts()[0].set_text( " GD, %d iterations" % min(i,len(gd_xs)-1) )
-        L.get_texts()[1].set_text( " Newton, %d iterations" % min(i,len(newton_xs)-1) )    
-        
+        L.get_texts()[1].set_text( " Newton, %d iterations" % min(i,len(newton_xs)-1) )
+
         plt.draw()
         input("Press Enter to continue...")

sqn/optimizer/sd_lbfgs.py
@@ -5,21 +5,28 @@ Created on Thu Apr 19 02:07:39 2018
 @author: vbigmouse
 """
 from numpy import dot, zeros, asarray, sqrt
+from math import isnan, isinf
 from scipy.optimize import OptimizeResult
+from time import time
+from pprint import pprint
 
-
-def harmonic_sequence(k):
+def harmonic_sequence(k, init_step_size):
     # For any nonnegative integer
-    return 0.9/(sqrt(k+1))
+    return init_step_size /(k+1)
+#    return 10 / (k+1)
+def sqrt_sequence(k, init_step_size):
+    # For any nonnegative integer
+    return init_step_size /(sqrt(k+1))
 
 class SdLBFGS():
     def __init__(self, func, initial_val, *args,
             max_iterations=1000,
             mem_size=10,
             batch_size=50,
+            init_step_size=0.1,
             step_size=harmonic_sequence,
-            delta=0.01,
-            tol=1e-4,
+            delta=0.1,
+            tol=1e-6,
             **kwargs):
 
         self._func = func
@@ -29,7 +36,8 @@ class SdLBFGS():
         self._max_mem_size = mem_size
         self._mem_size = 0
         self._batch_size = batch_size
-        self._step_size = step_size
+        self._step_size = lambda k:step_size(k, self._init_step_size)
+        self._init_step_size = init_step_size
         self._delta = delta
         self._tolerance = tol
 
@@ -38,12 +46,18 @@ class SdLBFGS():
         self._previous_val = zeros(self._initial_val.shape)
         self._current_grad = zeros(self._initial_val.shape)
         self._previous_grad = zeros(self._initial_val.shape)
+        self._current_objval = asarray(self._func(self._current_val)[0])
 
         self._backward_errors = ShiftList(self._max_mem_size)
         self._ybars = ShiftList(self._max_mem_size)
         self._rhos = ShiftList(self._max_mem_size)
 
         self._iteration_vals = [self._current_val]
+        self._iteration_grads = [self._current_grad]
+        self._iteration_objvals = [self._current_objval]
+
+        self._start_time = time()
+        self._iteration_runtimes = [0]
 
         self._result = OptimizeResult()
         self._result['success'] = False
@@ -52,22 +66,37 @@ class SdLBFGS():
 
     def run(self):
         while not self._result['success'] and self._iterations < self._max_iterations:
+#            pprint(self.__dict__, indent=2)
             self.sqn_step()
+            self._iteration_runtimes.append(time() - self._start_time)
+
+
         return self.result()
 
 
     def sqn_step(self):
         # compute stochastic gradient
+
+        value = zeros(self._current_objval.shape)
         grad = zeros(self._current_val.shape)
         for i in range(self._batch_size):
-            _, g = self._func(self._current_val)
+            v, g = self._func(self._current_val)
+#            print(v)
             grad += g
+            value += v
+
         grad /= self._batch_size
+        value /= self._batch_size
+#        grad = self._func(self._current_val)
         self._current_grad = grad
+        self._current_objval = value
+
 
 
         # stopping criterion, sends back to self.run()
-        print(dot(self._current_grad.T, self._current_grad))
+#        if self._iterations % 10 == 0:
+#            print(f'norm of grad:={dot(self._current_grad.T, self._current_grad)}')
+
         if dot(self._current_grad.T, self._current_grad) <= self._tolerance:
 
             self._result['success'] = True
@@ -79,6 +108,8 @@ class SdLBFGS():
         self._previous_val = self._current_val.copy()
         self._current_val -= self._step_size(self._iterations) * direction
         self._iteration_vals.append(self._current_val.copy())
+        self._iteration_grads.append(self._current_grad.copy())
+        self._iteration_objvals.append(self._current_objval)
         self._previous_grad = self._current_grad.copy()
         self._iterations += 1
 
@@ -89,21 +120,34 @@ class SdLBFGS():
 
         s = self._current_val - self._previous_val
         y = self._current_grad - self._previous_grad
-
+#        print(f's={s}, cur_val={self._current_val}')
+#        print(f'y={y}, cur_grad={self._current_grad}')
         # compute theta
         sTy = dot(s.T, y)
         yTy = dot(y.T, y)
         sTs = dot(s.T, s)
+        if sTy == 0 or isinf(yTy / sTy) or isnan(yTy / sTy):
+            gamma = self._delta
+        else:
+            gamma = max(yTy / sTy, self._delta)
 
-        gamma = max(yTy / sTy, self._delta)
 
         if sTy < 0.25 * gamma * sTs:
-            theta = 0.75 * gamma * sTs / (gamma * sTs - sTy)
+            theta = (0.75 * gamma * sTs / (gamma * sTs - sTy)).item(0)
+            rho = 1. / (0.25 * gamma * sTs)
         else:
             theta = 1.
-        #theta =1
+            if sTy == 0:
+                rho = 0
+            else:
+                rho = 1. / sTy
+
+#        theta =1
+
         y_bar = theta * y + ((1. - theta) * gamma) * s
-        rho = 1. / dot(s.T, y_bar)
+
+#        rho = 1. / dot(s.T, y_bar)
+
 
 
         if k == 0:
@@ -152,6 +196,9 @@ class SdLBFGS():
         self._result['njev'] = self._batch_size * self._iterations
         self._result['nit'] = self._iterations
         self._result['iteration_vals'] = self._iteration_vals
+        self._result['iteration_grads'] = self._iteration_grads
+        self._result['iteration_objvals'] = self._iteration_objvals
+        self._result['iteration_runtimes'] = self._iteration_runtimes
 
         return self._result
 

sqn/optimizer/test.py
@@ -1,8 +1,11 @@
-#import tensorflow as tf
+import tensorflow as tf
 from sqn_optimizer_hooks import SQNOptimizer
 from sd_lbfgs import SdLBFGS
-from hw1_functions import weird_func
+from sd_lbfgs import harmonic_sequence as harmonic_seq
+from sd_lbfgs import sqrt_sequence as sqrt_seq
 from hw4_functions import svm_objective_function_stochastic as svm_func
+from hw4_functions import svm_objective_function as svm_func_obj
+import matplotlib.pyplot as plt
 import numpy as np
 from time import sleep
 from pprint import pprint
@@ -12,51 +15,541 @@ def quadratic(x, order=1):
         return x * x
     if order==1:
         return x * x, 2 * x
+def norm_z(x):
+    x=(x-x.mean())/x.std()
+#    m=np.median(x)
+#    x.fillna(m,inplace=True)
+    return x
+
+def norm_m(x):
+    x = (x - x.min() + 1) / (x.max() - x.min())
+#    m = np.median(x)
+#    x.fillna(m, inplace=True)
+    return x
 
 #%%
-data = np.loadtxt('HIGGS_subset.csv', delimiter=',')
+rawdata = np.loadtxt('HIGGS_subset.csv', delimiter=',')
 
-labels = np.asmatrix(2*data[:,0]-1).T
-features = np.asmatrix(data[:,1:])
+labels = np.asmatrix(2 * rawdata[:, 0] -1).T
+features = np.asmatrix(rawdata[:, 1:])
+features = norm_z(features)
+
 data = np.concatenate((features, labels), axis = 1)
 d = features.shape[1]
 w = np.zeros((d,1))
 #%%
-
-#optimizer = SdLBFGS(lambda x: weird_func(x, order=1), 1., batch_size=2)
-
-
 optimizer = SdLBFGS(lambda x: svm_func(x, order=1, data=data, minibatch_size=50), w,
         batch_size=50)
 
-# for i in range(10):
-#     optimizer.sqn_step()
+result = optimizer.run()
+#%%
+'''Logistic Resression test'''
+
+'''General Setting'''
+max_iterations=1000
+points_to_plot=1000
+
+from sd_lbfgs import SdLBFGS
+from hw4_functions import cross_entropy_error as cross_enp_func
+
+#%%
+#labels = np.asmatrix(2 * rawdata[:, 0] -1).T
+labels = np.asmatrix(rawdata[:, 0]).T
+features = np.asmatrix(rawdata[:, 1:])
+features = norm_z(features)
+
+data = np.concatenate((features, labels), axis = 1)
+d = features.shape[1]
+w = np.zeros((d,1))
+
+
+''''''
+optimizer = SdLBFGS(lambda x: cross_enp_func(x, order=1, data=data, minibatch_size=1),
+                    w,
+                    batch_size=50,
+                    delta=0.01,
+                    init_step_size=0.001,
+                    maxiiterations=10000)
+result = optimizer.run()
+sdlbfs_x =result['iteration_vals']
+sdlbfs_values =result['iteration_objvals']
+sdlbfs_runtimes =result['iteration_runtimes']
+
+print('Solution found by sdLBFGS', sdlbfs_x[-1])
+print('Objective function', cross_enp_func(sdlbfs_x[-1], order=0, data=data, minibatch_size=len(labels)))
+
+#%%
+'SGD'
+from hw4_functions import cross_entropy_error as cross_enp_func
+import algorithms as alg
+obj_f = lambda x, order: cross_enp_func(x, order=order, data=data,  minibatch_size=50)
+initial_x=np.zeros((d,1))
+
+sgd_x, sgd_values, sgd_runtimes, sgd_xs = \
+    alg.subgradient_descent(obj_f, initial_x, max_iterations, 0.1)
+
+print('Solution found by stochastic subgradient descent', sgd_x)
+print('Objective function', obj_f(sgd_x,0))
+sgd_its = len(sgd_runtimes)
+sgd_values=[obj_f(sgd_xs[i],0) for i in range(0,sgd_its,int(sgd_its/min(sgd_its,points_to_plot)))]
+#%%
+'Ada'
+
+ada_x, ada_values, ada_runtimes, ada_xs = alg.adagrad( obj_f, initial_x, max_iterations, 0.1)
+print('Solution found by stochastic adagrad', ada_x)
+print('Objective function', obj_f(ada_x,0))
+
+ada_its = len(ada_runtimes)
+ada_values=[obj_f(ada_xs[i],0) for i in range(0, ada_its,int(ada_its/min(ada_its, points_to_plot)))]
+#%%
+'Subgradient descent'
+
+sd_x, sd_values, sd_runtimes, sd_xs = alg.subgradient_descent( obj_f, initial_x, max_iterations, 0.1)
+print('Solution found by subgradient descent', sd_x)
+print('Objective function', obj_f(sd_x,0))
+
+sd_its = len(sd_runtimes)
+sd_values=[obj_f(sd_xs[i],0) for i in range(0,sd_its,int(sd_its/min(sd_its, points_to_plot)))]
+#Obj func vs time
+
+
+#%%
+'BFGS'
+initial_x=np.zeros((d,1))
+init_h =np.zeros((d,d))
+obj_f = lambda x, order: cross_enp_func(x, order=order, data=data,  minibatch_size=1)
+bfgs_x, bfgs_values, bfgs_runtimes, bfgs_xs = alg.bfgs(obj_f, initial_x, init_h, maximum_iterations=20)
+
+print('Solution found by bfgs', bfgs_x)
+print('Objective function', obj_f(bfgs_x,0))
+bfgs_its = len(bfgs_runtimes)
+bfgs_values = [obj_f(bfgs_xs[i],0) for i in range(0,bfgs_its,int(bfgs_its/min(bfgs_its, points_to_plot)))]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#%%
+'''Weird Function test'''
+
+'''General Setting'''
+max_iterations=1000
+points_to_plot=1000
+#%%
+'SdLBFGS'
+from sd_lbfgs import SdLBFGS
+from hw1_functions import weird_func as weird_func
+
+initial_x = 0.1
+obj_f = lambda x: weird_func(x, order=1)
+optimizer = SdLBFGS(obj_f, initial_x, batch_size=50, delta=0.1, max_iterations=max_iterations)
+result = optimizer.run()
+
+sdlbfs_x =result['iteration_vals']
+sdlbfs_values =result['iteration_objvals']
+sdlbfs_runtimes =result['iteration_runtimes']
+
+print('Solution found by sdLBFGS', sdlbfs_x[-1])
+print('Objective function', obj_f(sdlbfs_x[-1],0))
+
+#%%
+'SGD'
+import algorithms as alg
+
+obj_f = lambda x, order: weird_func(x, order)
+
+sgd_x, sgd_values, sgd_runtimes, sgd_xs = \
+    alg.subgradient_descent(obj_f, initial_x, max_iterations, 0.1)
+
+print('Solution found by stochastic subgradient descent', sgd_x)
+print('Objective function', obj_f(sgd_x,0))
+sgd_its = len(sgd_runtimes)
+sgd_values=[obj_f(sgd_xs[i],0) for i in range(0,sgd_its,int(sgd_its/min(sgd_its,points_to_plot)))]
+#%%
+'Ada'
+
+ada_x, ada_values, ada_runtimes, ada_xs = alg.adagrad( obj_f, initial_x, max_iterations, 0.1)
+print('Solution found by stochastic adagrad', ada_x)
+print('Objective function', obj_f(ada_x,0))
+
+ada_its = len(ada_runtimes)
+ada_values=[obj_f(ada_xs[i],0) for i in range(0, ada_its,int(ada_its/min(ada_its, points_to_plot)))]
+#%%
+'Subgradient descent'
+
+sd_x, sd_values, sd_runtimes, sd_xs = alg.subgradient_descent( obj_f, initial_x, max_iterations, 0.1)
+print('Solution found by subgradient descent', sd_x)
+print('Objective function', obj_f(sd_x,0))
+
+sd_its = len(sd_runtimes)
+sd_values=[obj_f(sd_xs[i],0) for i in range(0,sd_its,int(sd_its/min(sd_its, points_to_plot)))]
+#Obj func vs time
+
+
+#%%
+'BFGS'
+
+init_h = 0.1
+bfgs_x, bfgs_values, bfgs_runtimes, bfgs_xs = alg.bfgs(obj_f, initial_x, init_h, maximum_iterations=max_iterations)
+
+print('Solution found by bfgs', bfgs_x)
+print('Objective function', obj_f(bfgs_x,0))
+bfgs_its = len(bfgs_runtimes)
+bfgs_values = [obj_f(bfgs_xs[i],0) for i in range(0,bfgs_its,int(bfgs_its/min(bfgs_its, points_to_plot)))]
+
+
+#%%
+'''plot for weird function'''
+plt.figure(figsize=(14,8))
+#line_sdlbfgs, = plt.semilogx(np.array([i for i in range(len(sdlbfs_runtimes))]),
+'''num of iterations to object function values'''
+line_sdlbfgs, = plt.semilogx(np.array([i for i in range(len(sdlbfs_runtimes))]),
+                         np.array(sdlbfs_values), linewidth=2, color='k', dashes = [1, 1],
+                         marker='.', label='SdLBFGS')
+
+
+#line_sgd, = plt.semilogx(np.array(sgd_runtimes[0::int(sgd_its/min(sgd_its, points_to_plot))]),
+line_sgd, = plt.semilogx(np.array([i for i in range(len(sgd_runtimes))]),
+                         np.array(sgd_values).flatten(), linewidth=2, color='r', dashes = [1, 1],
+                         marker='o', label='SGD')
+#line_sd, = plt.semilogx(sd_runtimes[0::int(sd_its/min(sd_its, points_to_plot))],
+line_sd, = plt.semilogx(np.array([i for i in range(len(sd_runtimes))]),
+                        np.array(sd_values).flatten(), linewidth=2, color='g',
+                        marker='*', label='SD')
+
+#line_ada, = plt.semilogx(ada_runtimes[0::int(sgd_its/min(ada_its, points_to_plot))],
+line_ada, = plt.semilogx(np.array([i for i in range(len(ada_runtimes))]),
+                        np.array(ada_values).flatten(), linewidth=2, color='b', dashes = [4, 1],
+                        marker='x', label='AdaGrad')
+
+#line_bfgs = plt.semilogx(bfgs_runtimes[0::int(bfgs_its/min(bfgs_its, points_to_plot))],
+line_bfgs = plt.semilogx(np.array([i for i in range(len(bfgs_runtimes))]),
+                        np.array(bfgs_values).flatten(), linewidth=2, color='g', dashes = [4, 1],
+                        marker='x', label='BFGS')
+
+plt.legend()
+plt.show()
+
+#%%
+'''non-convex function'''
+
+points_to_plot=1000
+max_iterations = 10000
+
+from hw1_functions import no_conv_func2
+obj_f = lambda x : no_conv_func2(x, order=1)
+
+
+initial_x = 2.
+
+optimizer = SdLBFGS(obj_f, initial_x,
+                    batch_size=1,
+                    delta=0.01,
+                    step_size=sqrt_seq,
+#                    step_size=harmonic_seq,
+                    mem_size=20,
+                    init_step_size=0.1,
+                    max_iterations=10000)
 
 result = optimizer.run()
-import matplotlib.pyplot as plt
+
+sdlbfs_x =result['iteration_vals']
+sdlbfs_values =result['iteration_objvals']
+sdlbfs_runtimes =result['iteration_runtimes']
+
+
+print('Solution found by sdLBFGS', sdlbfs_x[-1])
+print('Objective function', no_conv_func2(sdlbfs_x[-1],0))
+
+sdlbfs_x =sdlbfs_x[0::int(len(sdlbfs_x)/min(len(sdlbfs_runtimes), points_to_plot))]
+sdlbfs_values =sdlbfs_values[0::int(len(sdlbfs_values)/min(len(sdlbfs_runtimes), points_to_plot))]
+sdlbfs_runtimes =sdlbfs_runtimes[0::int(len(sdlbfs_runtimes)/min(len(sdlbfs_runtimes), points_to_plot))]
+
+'SGD'
+import algorithms as alg
+
+obj_f = lambda x, order: no_conv_func2(x, order)
+initial_x = 2.
+sgd_x, sgd_values, sgd_runtimes, sgd_xs = \
+    alg.subgradient_descent(obj_f, initial_x, max_iterations, 0.01)
+
+print('Solution found by stochastic subgradient descent', sgd_x)
+print('Objective function', obj_f(sgd_x,0))
+sgd_its = len(sgd_runtimes)
+sgd_values=[obj_f(sgd_xs[i],0) for i in range(0,sgd_its,int(sgd_its/min(sgd_its,points_to_plot)))]
 #%%
-from hw4_functions import svm_objective_function as svm_func_obj
+'Ada'
+obj_f = lambda x, order: no_conv_func2(x, order)
+ada_x, ada_values, ada_runtimes, ada_xs = alg.adagrad( obj_f, initial_x, max_iterations, 0.1)
+print('Solution found by stochastic adagrad', ada_x)
+print('Objective function', obj_f(ada_x,0))
+
+ada_its = len(ada_runtimes)
+ada_values=[obj_f(ada_xs[i],0) for i in range(0, ada_its,int(ada_its/min(ada_its, points_to_plot)))]
+#%%
+'BFGS'
+obj_f = lambda x, order: no_conv_func2(x, order)
+init_h = 0.1
+bfgs_x, bfgs_values, bfgs_runtimes, bfgs_xs = alg.bfgs(obj_f, initial_x, init_h, maximum_iterations=max_iterations)
+
+print('Solution found by bfgs', bfgs_x)
+print('Objective function', obj_f(bfgs_x,0))
+bfgs_its = len(bfgs_runtimes)
+bfgs_values = [obj_f(bfgs_xs[i],0) for i in range(0,bfgs_its,int(bfgs_its/min(bfgs_its, points_to_plot)))]
+
+
+#%%
+plt.figure(figsize=(14,8))
+
+'''num of iterations to object function values'''
+sdlbfgs_X = np.array([i for i in range(0, len(sdlbfs_values))])
+#sdlbfgs_X = np.array([i for i in range(len(sdlbfs_runtimes))])
+sdlbfgs_Y = np.array(sdlbfs_values)
+line_sdlbfgs, = plt.semilogx( sdlbfgs_X, sdlbfgs_Y, linewidth=2, color='k', dashes = [1, 1],
+                         marker='.', label='SdLBFGS')
+
+sgd_X = np.array([i for i in range(len(sgd_values))])
+#sgd_X = np.array([i for i in range(0, len(sgd_runtimes),int(len(sgd_runtimes)/points_to_plot))])
+sgd_Y = np.array(sgd_values).reshape(sgd_X.shape)
+#line_sgd, = plt.semilogx(np.array(sgd_runtimes[0::int(sgd_its/min(sgd_its, points_to_plot))]),
+line_sgd, = plt.semilogx(sgd_X,sgd_Y, linewidth=2, color='r', dashes = [1, 1],
+                         marker='o', label='SGD')
+
+#ada_X = np.array([i for i in range(0, len(ada_runtimes),int(len(ada_runtimes)/points_to_plot))])
+ada_X = np.array([i for i in range(len(ada_values))])
+ada_Y = np.array(ada_values).reshape(ada_X.shape)
+#line_ada, = plt.semilogx(ada_runtimes[0::int(sgd_its/min(ada_its, points_to_plot))],
+line_ada, = plt.semilogx(ada_X, ada_Y, linewidth=2, color='b', dashes = [4, 1],
+                        marker='x', label='AdaGrad')
+
+#bfgs_X = np.array([i for i in range(0, len(bfgs_runtimes),int(len(bfgs_runtimes)/points_to_plot))])
+bfgs_X = np.array([i for i in range(len(bfgs_values))])
+bfgs_Y = np.array(bfgs_values).reshape(bfgs_X.shape)
+#line_bfgs = plt.semilogx(bfgs_runtimes[0::int(bfgs_its/min(bfgs_its, points_to_plot))],
+line_bfgs = plt.semilogx(bfgs_X, bfgs_Y, linewidth=2, color='g', dashes = [4, 1],
+                        marker='x', label='BFGS')
+
+plt.title(r'non-convex function $x^{6}-2x^{5}+x^{3}-x^{2}+3x$')
+plt.legend()
+plt.xlabel('Iteration')
+plt.ylabel('Objective function value')
+plt.show()
+#%%
+plt.figure(figsize=(14,8))
+
+'''num of iterations to object function values'''
+sdlbfgs_X = np.array([i for i in range(0, len(sdlbfs_values))])
+#sdlbfgs_X = np.array([i for i in range(len(sdlbfs_runtimes))])
+sdlbfgs_Y = np.array(sdlbfs_values)
+line_sdlbfgs, = plt.semilogx( sdlbfgs_X, sdlbfgs_Y, linewidth=2, color='k', dashes = [1, 1],
+                         marker='.', label='SdLBFGS')
+
+sgd_X = np.array([i for i in range(len(sgd_values))])
+#sgd_X = np.array([i for i in range(0, len(sgd_runtimes),int(len(sgd_runtimes)/points_to_plot))])
+sgd_Y = np.array(sgd_values).reshape(sgd_X.shape)
+#line_sgd, = plt.semilogx(np.array(sgd_runtimes[0::int(sgd_its/min(sgd_its, points_to_plot))]),
+line_sgd, = plt.semilogx(sgd_X,sgd_Y, linewidth=2, color='r', dashes = [1, 1],
+                         marker='o', label='SGD')
+
+#ada_X = np.array([i for i in range(0, len(ada_runtimes),int(len(ada_runtimes)/points_to_plot))])
+ada_X = np.array([i for i in range(len(ada_values))])
+ada_Y = np.array(ada_values).reshape(ada_X.shape)
+#line_ada, = plt.semilogx(ada_runtimes[0::int(sgd_its/min(ada_its, points_to_plot))],
+line_ada, = plt.semilogx(ada_X, ada_Y, linewidth=2, color='b', dashes = [4, 1],
+                        marker='x', label='AdaGrad')
+
+#bfgs_X = np.array([i for i in range(0, len(bfgs_runtimes),int(len(bfgs_runtimes)/points_to_plot))])
+bfgs_X = np.array([i for i in range(len(bfgs_values))])
+bfgs_Y = np.array(bfgs_values).reshape(bfgs_X.shape)
+#line_bfgs = plt.semilogx(bfgs_runtimes[0::int(bfgs_its/min(bfgs_its, points_to_plot))],
+line_bfgs = plt.semilogx(bfgs_X, bfgs_Y, linewidth=2, color='g', dashes = [4, 1],
+                        marker='x', label='BFGS')
+
+plt.title(r'non-convex function $x^{6}-2x^{5}+x^{3}-x^{2}+3x$')
+plt.legend()
+plt.xlabel('Iteration')
+plt.ylabel('Objective function value')
+plt.show()
+#%%
+'''plot object function'''
+plt.figure(figsize=(14,8))
+pltX = np.array([i/10 for i in range(-15, 20, 1)])
+
+pltY = no_conv_func2(pltX, 0)
+plt.plot(pltX, pltY)
+plt.title(r'non-convex function $x^{6}-2x^{5}+x^{3}-x^{2}+3x$')
+plt.legend()
+plt.xlabel('Iteration')
+plt.ylabel('Objective function value')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#%%
+'''tanh obj test'''
+
+'''General Setting'''
+max_iterations=1000
+points_to_plot=1000
+
+from sd_lbfgs import SdLBFGS
+from hw4_functions import tanh_regul_error as tanh_obj
+
+#%%
+rawdata = np.loadtxt('HIGGS_subset.csv', delimiter=',')
+
+labels = np.asmatrix(2 * rawdata[:, 0] -1).T
+features = np.asmatrix(rawdata[:, 1:])
+features = norm_z(features)
+
+data = np.concatenate((features, labels), axis = 1)
+d = features.shape[1]
+w = np.zeros((d,1))
+
+
+
+optimizer = SdLBFGS(lambda x: tanh_obj(x, order=1, data=data, minibatch_size=1),
+                    initial_val=w,
+                    batch_size=10,
+                    delta=0.01,
+                    step_size=harmonic_seq,
+                    mem_size=20,
+                    init_step_size=0.01,
+                    max_iterations=1000)
+result = optimizer.run()
+sdlbfs_x =result['iteration_vals']
+sdlbfs_values =result['iteration_objvals']
+sdlbfs_runtimes =result['iteration_runtimes']
+sdlbfs_grads = result['iteration_grads']
+
+print('Solution found by sdLBFGS', sdlbfs_x[-1])
+print('Objective function', tanh_obj(sdlbfs_x[-1], order=0, data=data, minibatch_size=len(labels)))
+
+
+plt.figure(figsize=(14,8))
+#line_sdlbfgs, = plt.semilogx(np.array([i for i in range(len(sdlbfs_runtimes))]),
+'''num of iterations to object function values'''
+pltX = np.array([i for i in range(len(sdlbfs_runtimes))])
+#pltY = np.array(sdlbfs_values).reshape(pltX.shape)
+
+pltY = np.linalg.norm(sdlbfs_grads, axis=1).reshape(pltX.shape)
+line_sdlbfgs = plt.semilogx(pltX, pltY, linewidth=2, color='k', dashes = [1, 1],
+                         marker='.', label='SdLBFGS')
+plt.legend()
+plt.grid()
+plt.show()
+
+
+#%%
+'SGD'
+from hw4_functions import cross_entropy_error as cross_enp_func
+import algorithms as alg
+obj_f = lambda x, order: cross_enp_func(x, order=order, data=data,  minibatch_size=50)
+initial_x=np.zeros((d,1))
+
+sgd_x, sgd_values, sgd_runtimes, sgd_xs = \
+    alg.subgradient_descent(obj_f, initial_x, max_iterations, 0.1)
+
+print('Solution found by stochastic subgradient descent', sgd_x)
+print('Objective function', obj_f(sgd_x,0))
+sgd_its = len(sgd_runtimes)
+sgd_values=[obj_f(sgd_xs[i],0) for i in range(0,sgd_its,int(sgd_its/min(sgd_its,points_to_plot)))]
+#%%
+'Ada'
+
+ada_x, ada_values, ada_runtimes, ada_xs = alg.adagrad( obj_f, initial_x, max_iterations, 0.1)
+print('Solution found by stochastic adagrad', ada_x)
+print('Objective function', obj_f(ada_x,0))
+
+ada_its = len(ada_runtimes)
+ada_values=[obj_f(ada_xs[i],0) for i in range(0, ada_its,int(ada_its/min(ada_its, points_to_plot)))]
+#%%
+'Subgradient descent'
+
+sd_x, sd_values, sd_runtimes, sd_xs = alg.subgradient_descent( obj_f, initial_x, max_iterations, 0.1)
+print('Solution found by subgradient descent', sd_x)
+print('Objective function', obj_f(sd_x,0))
+
+sd_its = len(sd_runtimes)
+sd_values=[obj_f(sd_xs[i],0) for i in range(0,sd_its,int(sd_its/min(sd_its, points_to_plot)))]
+#Obj func vs time
+
+
+#%%
+'BFGS'
+initial_x=np.zeros((d,1))
+init_h =np.zeros((d,d))
+obj_f = lambda x, order: cross_enp_func(x, order=order, data=data,  minibatch_size=1)
+bfgs_x, bfgs_values, bfgs_runtimes, bfgs_xs = alg.bfgs(obj_f, initial_x, init_h, maximum_iterations=20)
+
+print('Solution found by bfgs', bfgs_x)
+print('Objective function', obj_f(bfgs_x,0))
+bfgs_its = len(bfgs_runtimes)
+bfgs_values = [obj_f(bfgs_xs[i],0) for i in range(0,bfgs_its,int(bfgs_its/min(bfgs_its, points_to_plot)))]
+
+
+
+
+
+
+#%%
+
 x = np.array(result['iteration_vals'])
 vq = np.vectorize(lambda x: svm_func_obj(x, order=0, data=data))
 
-x = np.array(result['iteration_vals'])
-vq = np.vectorize(weird_func)
-y = vq(x, order=0)
+#optimizer = SdLBFGS(lambda x: weird_func(x, order=1), 1., batch_size=2)
+#%%
+
+#x = np.array(result['iteration_vals'])
+#vq = np.vectorize(weird_func)
+#y = vq(x, order=0)
 
 
-# vector = tf.Variable([7., 7.], 'vector')
-#
-# # Make vector norm as small as possible.
-# loss = tf.reduce_sum(tf.square(vector))
-#
-# print(loss)
-#
-# optimizer = SQNOptimizer(loss)
-#
-# with tf.Session() as session:
-#     print("Inside the session.")
-#     session.run(tf.global_variables_initializer())
-#
-#     optimizer.minimize(session)
-#
-#     # The value of vector should now be [0., 0.].
+vector = tf.Variable([7., 7.], 'vector')
+
+ # Make vector norm as small as possible.
+loss = tf.reduce_sum(tf.square(vector))
+print(loss)
+optimizer = SQNOptimizer(loss)
+
+with tf.Session() as session:
+    print("Inside the session.")
+    session.run(tf.global_variables_initializer())
+    optimizer.minimize(session)
+     # The value of vector should now be [0., 0.].